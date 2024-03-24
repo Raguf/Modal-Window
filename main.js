a566c018197d7b6e7727109339ae0efb96705fb0
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-25 02:29:49 +0400

    Create modal window

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {}
+console.log(btnsOpenModal[i].textContent);
