199b94a92d4deff2da178e288dd36c92f9e3d740
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-25 03:24:42 +0400

    Working with some classes

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 'use strict';
+/*---------- Common elements---------- */
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
-console.log(btnsOpenModal);
+/*---------- Open modal function start---------- */
 
-for (let i = 0; i < btnsOpenModal.length; i++) {}
-console.log(btnsOpenModal[i].textContent);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+/*---------- Close  modal function start---------- */
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
